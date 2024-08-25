747bfd236521900ecfb99f0ce365ab10389fdee6
m7md3atef <m7md3atef@yahoo.com>
2024-08-26 02:27:38 +0300

    0x16-api_advanced Task

--- 0x16-api_advanced/2-main.py
+++ 0x16-api_advanced/2-main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-2-main file
+2-main
 """
 import sys
 
 if __name__ == '__main__':
     recurse = __import__('2-recurse').recurse
     if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search. ")
+        print("Please pass an argument for the subreddit to search.")
     else:
         result = recurse(sys.argv[1])
         if result is not None:
